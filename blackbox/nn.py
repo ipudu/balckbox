c7aa8ab38ba3629bcdf8d3a3a87ddc676c021245
Pu Du <rocketsboy@gmail.com>
2017-09-07 17:45:51 -0500

    G1 to G4

--- blackbox/nn.py
+++ blackbox/nn.py
@@ -41,10 +41,18 @@ def G1(Rij, Rc):
     """type 1 of fingerprint"""
     return fc(Rij, Rc)
 
-def G2(Rij, Rc):
+def G2(Rij, Rc, Rs, etha):
     """type 1 of fingerprint"""
-    pass
+    return np.exp(-eta * (Rij - Rs) ** 2. / (Rc ** 2)) * fc(Rij, Rc)
 
-def G4(Rij, Rc, ):
+def G3(Rij, Rc, kappa):
+    """type 3 of fingerprint"""
+    return np.cos(kappa * Rij) * fc(Rij, Rc)
+
+def G4(Rij, Rik, Rjk, Rc, Rs, zeta, lmb, theta, etha):
     """type 4 of fingerprint"""
+    return (1 + lmb * theta) * np.exp(-etha * (Rij ** 2. + Rik ** 2. + Rjk ** 2.) * \
+            fc(Rij, Rc) * fc(Rik, Rc) * fc(Rjk, Rc)
+
+def G5():
     pass
